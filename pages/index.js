d051a0a65f433e06c977f517a345db28ea76d9f1
0iqbeginner <86001972+Daulatraam@users.noreply.github.com>
2022-09-06 20:36:17 +0530

    deleted useless files

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ const submit=()=>{
     
           <div className='dm:hidden flex items-center justify-center w-[30%]'>  
             <button onClick={menuClick} className=' bg-black w-10 h-10 rounded-lg justify-center flex item-center'>          <svg className="block self-center h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="white" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+              <path stroke-linecap="round" stroke-LineJoin="round" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
 </button>
                   
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,19 +25,19 @@ module.exports = {
       
       
       'lx2': { 'min': '1535px' },
-      // => @media (max-width: 1535px) { ... }
+      // => @media (min-width: 1535px) { ... }
 
       'lx': { 'min': '1279px' },
-      // => @media (max-width: 1279px) { ... }
+      // => @media (min-width: 1279px) { ... }
 
       'gl': { 'min': '1023px' },
-      // => @media (max-width: 1023px) { ... }
+      // => @media (min-width: 1023px) { ... }
 
       'dm': { 'min': '767px' },
-      // => @media (max-width: 767px) { ... }
+      // => @media (min-width: 767px) { ... }
 
       'ms': { 'min': '639px' },
-      // => @media (max-width: 639px) { ... }
+      // => @media (min-width: 639px) { ... }
     },
 
     extend: {},
